4cb4d5c005472eee2ce4d0c96fc40701ce3de6f4
Grimm <85710701+GrimmHX@users.noreply.github.com>
2022-03-31 16:25:06 -0400

    ESP Core

---
+++ secure.js
@@ -0,0 +1,18 @@
+//Codes - 500 Full Access - 501 Semi Outage - 502 Full outage
+
+(function() {
+
+    code = 502
+
+    if (code == "500") {
+        return;
+    }
+
+    if (code == "501") {
+        window.location.replace("https://esp-core.netlify.app/urgent/esp_anc_502");
+    }
+    
+    if (code == "502") {
+        window.location.replace("https://esp-core.netlify.app/urgent/esp_anc_502");
+    }
+})()
